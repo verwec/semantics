d771216902e5743c948aa7fbc982dd71517ead31
verwec <sebastian@vertragswecker.de>
2016-04-29 16:47:44 +0200

    Allow to create things

--- lib/semantics/thing.rb
+++ lib/semantics/thing.rb
@@ -2,17 +2,33 @@ module Semantics
   class Thing
     include HTTParty
 
-    attr_reader :id, :generated_text
+    attr_reader :id, :generated_text, :name
 
     def initialize(attributes)
       @id = attributes["id"]
       @generated_text = attributes["generated_text"]
+      @name = attributes["name"]
     end
 
     def self.find(cp_id, obj_id)
       endpoint = "https://api.ax-semantics.com/v1/content-project/#{cp_id}/thing/#{obj_id}"
       options = { headers: headers }
       attributes = get(endpoint, options)
+      new(attributes)
+    end
+
+    def self.create(cp_id, uid, name, pure_data)
+      endpoint = "https://api.ax-semantics.com/v1/content-project/#{cp_id}/thing/"
+      options = {
+        headers: headers,
+        body: {
+          uid: Time.now.to_s,
+          name: name,
+          content_project: cp_id,
+          pure_data: pure_data
+        }.to_json
+      }
+      attributes = post(endpoint, options)
       new(attributes)
     end
 
--- spec/lib/semantics/thing_spec.rb
+++ spec/lib/semantics/thing_spec.rb
@@ -9,4 +9,15 @@ describe Semantics::Thing do
       expect(thing.generated_text).to include 'Welche Leistungen bietet'
     end
   end
+
+  describe '#create' do
+    it 'creates a thing' do
+      cp_id = 4591
+      uid = 112343
+      name = 'foobar'
+      pure_date = { key: 'value' }
+      thing = Semantics::Thing.create(cp_id, uid, name, pure_date)
+      expect(thing.name).to eq(name)
+    end
+  end
 end
