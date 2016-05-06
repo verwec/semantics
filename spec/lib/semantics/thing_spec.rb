102a086ab8a6b319daccad76381e1c58c9d23c78
verwec <sebastian@vertragswecker.de>
2016-05-06 16:58:41 +0200

    Improve specs
    * use fixtures instead of vcd
    * fix spec names

--- spec/lib/semantics/thing_spec.rb
+++ spec/lib/semantics/thing_spec.rb
@@ -1,72 +1,61 @@
 require 'spec_helper'
 
 describe Semantics::Thing do
-  let(:cp_id) { 4748 }
-  let(:obj_id) { 93_685 }
+  let(:cp_id) { 123 }
+  let(:obj_id) { 1 }
 
-  describe '#all' do
+  describe '.all' do
     it 'lists all things' do
-      VCR.use_cassette('all_content_projects') do
-        expect(Semantics::Thing.all(cp_id).count).to be > 0
-      end
+      things = Semantics::Thing.all(cp_id)
+      expect(things.count).to eq(1)
+      expect(things.first).to be_kind_of(Semantics::Thing)
     end
   end
 
-  describe '#find' do
-    let(:thing) do
-      VCR.use_cassette('get_thing') do
-        Semantics::Thing.find(cp_id, obj_id)
-      end
-    end
-    subject { thing }
+  describe '.find' do
+    subject { Semantics::Thing.find(cp_id, obj_id) }
 
     describe 'the found thing' do
       its('status') { is_expected.to eq 'not requested' }
-      its('created') { is_expected.to be }
-      its('modified') { is_expected.to be }
-      its('uid') { is_expected.to be }
+      its('created') { is_expected.to eq '2016-05-02T09:19:07.172345Z' }
+      its('modified') { is_expected.to eq '2016-05-02T09:19:07.172636Z' }
+      its('uid') { is_expected.to eq '123' }
       its('name') { is_expected.to eq 'foobar' }
       its('pure_data') { is_expected.to eq('key' => 'value') }
       its('content_project') { is_expected.to eq cp_id }
     end
 
-    it 'throws an appropriated error' do
-      VCR.use_cassette('get_thing_error') do
-        expect { Semantics::Thing.find(1, nil) }.to raise_error Semantics::ApiError
-      end
+    it 'can throw an appropriated error' do
+      expect do
+        Semantics::Thing.find(1, nil)
+      end.to raise_error Semantics::ApiError
     end
   end
 
-  describe '#create' do
+  describe '.create' do
     it 'creates a thing' do
-      VCR.use_cassette('create_thing') do
-        uid = Time.now.to_s
-        name = 'foobar'
-        pure_date = { key: 'value' }
-        thing = Semantics::Thing.create(cp_id, uid, name, pure_date)
-        expect(thing.name).to eq(name)
-      end
+      uid = 1
+      pure_date = { key: 'value' }
+      name =  'foobar'
+      thing = Semantics::Thing.create(cp_id, uid, name, pure_date)
+      expect(thing).to be_kind_of(Semantics::Thing)
     end
   end
 
-  describe '#update' do
+  describe '.update' do
     it 'updates a thing' do
-      VCR.use_cassette('update_thing') do
-        name = 'foobar'
-        uid = 214
-        pure_date = { name: 'Bob' }
-        thing = Semantics::Thing.update(cp_id, obj_id, uid, name, pure_date)
-        expect(thing.name).to eq(name)
-      end
+      name = 'foobar'
+      pure_date = { name: 'Bob' }
+      uid = 1
+      thing = Semantics::Thing.update(cp_id, obj_id, uid, name, pure_date)
+      expect(thing).to be_kind_of(Semantics::Thing)
     end
   end
 
-  describe '#generate_content' do
-    it 'generates content for a single object' do
-      VCR.use_cassette('generate_content') do
-        response = Semantics::Thing.generate_content(cp_id, obj_id)
-        expect(response).to eq 'NOT_CALLED'
-      end
+  describe '.generate_content' do
+    it 'generates content' do
+      status = Semantics::Thing.generate_content(cp_id, obj_id)
+      expect(status).to eq 'SUCCESS'
     end
   end
 end
--- spec/support/fake_ax_semantics.rb
+++ spec/support/fake_ax_semantics.rb
@@ -1,9 +1,47 @@
 require 'sinatra/base'
 
 class FakeAxSemantics < Sinatra::Base
-
+  # login
   post '/v1/rest-auth/login/' do
     json_response 200, 'login.json'
+  end
+
+  # content_projects
+  get '/v1/content-project/' do
+    json_response 200, 'content_projects.json'
+  end
+
+  get '/v1/content-project/:id/' do
+    json_response 200, 'content_project.json'
+  end
+
+  post '/v1/content-project/' do
+    json_response 201, 'content_project.json'
+  end
+
+  # things
+  get '/v1/allthings/' do
+    json_response 200, 'things.json'
+  end
+
+  get '/v1/content-project/123/thing/1/' do
+    json_response 200, 'thing.json'
+  end
+
+  get '/v1/content-project/123/thing//' do
+    json_response 404
+  end
+
+  post '/v1/content-project/123/thing/' do
+    json_response 201, 'thing.json'
+  end
+
+  put '/v1/content-project/123/thing/1/' do
+    json_response 200, 'thing.json'
+  end
+
+  post '/v1/content-project/123/thing/1/generate_content/' do
+    json_response 200, 'generate_content.json'
   end
 
   private
